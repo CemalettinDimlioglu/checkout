710df25798054488c9e95fb62f70028f85f1e647
CMLTTN <cemaldim@gmail.com>
2023-03-15 17:07:58 +0300

    first commit

---
+++ 004-Checkout-Page/data.js
@@ -0,0 +1,32 @@
+const data = [
+  {
+    id: 1,
+    name: "Macbook M1 Air 16gb",
+    image:
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-air-space-gray-select-201810?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1664472289661",
+    price: 1100,
+    dampingRate: 0.8,
+    amount: 1,
+  },
+  {
+    id: 2,
+    name: "Apple Magic Keyboard",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_sN_hYcMS94A7LWSjTawXliN1CkG2uzm-HwSC6nBZCBPiyvuHzVd0ZVq8BrsqDLVd8LE&usqp=CAU",
+    price: 89.99,
+    dampingRate: 0.8,
+    amount: 1,
+  },
+  {
+    id: 3,
+    name: "Apple Magic Mouse",
+    image:
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MK2E3?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1626468075000",
+    price: 69.99,
+    dampingRate: 0.8,
+    amount: 1,
+  },
+];
+
+export default data;
+
